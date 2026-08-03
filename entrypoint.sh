f190df844122a0591b554339d553d3614e80d45a
Ahmad Arshad <ahmadarshad12345@gmail.com>
2026-08-03 15:39:46 +0100

    updated entrypoint to first download the model in the production setup and then start the api

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,10 +16,17 @@ echo "========================================"
 # Production Mode
 # ----------------------------
 if [ "$ENVIRONMENT" = "production" ]; then
+
     echo "Production environment detected."
+    # python -u test.py 
+    echo "Downloading latest model..."
+
+    python -u download_model.py
+
     echo "Starting API..."
 
     exec python -u api.py
+
 fi
 
 # ----------------------------
